1ce9252233c4479a6659a0c9d76c3932c8c7f181
jalejo-cyber <jalejo@fomentformacio.com>
2026-03-09 16:37:37 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,12 @@
-import './globals.css'
-import Navbar from '../components/Navbar'
+import "../styles/globals.css"
+import Navbar from "../components/Navbar"
 
 export default function RootLayout({ children }) {
-
   return (
     <html>
       <body>
 
-        <Navbar/>
+        <Navbar />
 
         <div className="container">
           {children}
