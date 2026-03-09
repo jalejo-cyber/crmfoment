8b1ab5b4b2e7bd4ba25bd1ae3f8f5b0d134ffe45
jalejo-cyber <jalejo@fomentformacio.com>
2026-03-09 16:00:26 +0100

    Update navbar.js

--- components/navbar.js
+++ components/navbar.js
@@ -1,1 +1,20 @@
+"use client"
 
+import Link from "next/link"
+
+export default function Navbar(){
+
+  return(
+    <div className="navbar">
+
+      <Link href="/">Dashboard</Link>
+
+      <Link href="/persones">Persones</Link>
+
+      <Link href="/empreses">Empreses</Link>
+
+      <Link href="/programes">Programes</Link>
+
+    </div>
+  )
+}
