9580c8f1a8c40801cc451dd662e50492462af712
jalejo-cyber <jalejo@fomentformacio.com>
2026-03-09 16:42:48 +0100

    Update page.js

--- app/programes/page.js
+++ app/programes/page.js
@@ -1,1 +1,53 @@
+"use client"
 
+import { useEffect, useState } from "react"
+import { supabase } from "../../lib/supabase"
+
+export default function Programes() {
+
+  const [programes,setProgrames] = useState([])
+
+  useEffect(()=>{
+    load()
+  },[])
+
+  async function load(){
+
+    const {data} = await supabase
+      .from("programes")
+      .select("*")
+
+    setProgrames(data || [])
+
+  }
+
+  return (
+
+    <div>
+
+      <h1>Programes</h1>
+
+      <table>
+
+        <thead>
+          <tr>
+            <th>Nom</th>
+          </tr>
+        </thead>
+
+        <tbody>
+
+          {programes.map(p => (
+            <tr key={p.id}>
+              <td>{p.nom}</td>
+            </tr>
+          ))}
+
+        </tbody>
+
+      </table>
+
+    </div>
+
+  )
+}
